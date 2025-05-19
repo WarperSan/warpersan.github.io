3481335f7880e5e1784ec3d1d649c8af6d7f9335
WarperSan <leumas.ecole@gmail.com>
2025-05-19 18:09:37 -0400

    Update

--- scripts/elements/WebGLElement.js
+++ scripts/elements/WebGLElement.js
@@ -1,6 +1,31 @@
+function onPlayerLoaded(player, foreground) {
+    if (!player.hasAttribute("src"))
+        return;
+
+    const iframeDoc = player.contentDocument || player.contentWindow.document;
+    const game = iframeDoc.querySelector("canvas");
+
+    game.addEventListener("blur", () => {
+        foreground.style.display = "block";
+        foreground.style.pointerEvents = "none";
+    });
+
+    game.addEventListener("focus", () => {
+        foreground.style.display = "none";
+    });
+
+    game.classList.add("web-game-content");
+
+    game.style.maxWidth = "100%";
+    game.style.maxHeight = "100%";
+    game.style.borderRadius = "1rem";
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelectorAll('webgl').forEach(el => {
-        const src = el.getAttribute("src") || "";
+        const src = el.getAttribute("src");
+        const mobile = el.getAttribute("mobile");
+        const pc = el.getAttribute("pc");
 
         const container = document.createElement("div");
         const player = document.createElement("iframe");
@@ -14,37 +39,25 @@ document.addEventListener('DOMContentLoaded', () => {
         player.setAttribute("allowtransparency", true);
         player.setAttribute("frameborder", 0);
         player.setAttribute("scrolling", "no");
-        player.addEventListener("load", () => {
-            if (!player.hasAttribute("src"))
-                return;
-
-            const iframeDoc = player.contentDocument || player.contentWindow.document;
-            const game = iframeDoc.querySelector("canvas");
-
-            game.addEventListener("blur", () => {
-                foreground.style.display = "block";
-                foreground.style.pointerEvents = "none";
-            });
-
-            game.addEventListener("focus", () => {
-                foreground.style.display = "none";
-            });
-
-            game.classList.add("web-game-content");
-
-            game.style.maxWidth = "100%";
-            game.style.maxHeight = "100%";
-            game.style.borderRadius = "1rem";
-        });
+        player.addEventListener("load", () => onPlayerLoaded(player, foreground));
 
         foreground.title = "Click to Play";
         foreground.classList.add("web-game-foreground");
-        foreground.addEventListener("click", e => {
-            if (!player.hasAttribute("src"))
-                player.setAttribute("src", src);
 
-            foreground.style.display = "none";
-        });
+        const isMobile =  window.mobileCheck && window.mobileCheck();
+        const isPlayable = isMobile && mobile != null || !isMobile && pc != null;
+
+        if (isPlayable) {
+            foreground.classList.add("play");
+            foreground.addEventListener("click", e => {
+                if (!player.hasAttribute("src"))
+                    player.setAttribute("src", src);
+
+                foreground.style.display = "none";
+            });
+        } else {
+            foreground.classList.add("error");
+        }
 
         container.appendChild(player);
         container.appendChild(foreground);
