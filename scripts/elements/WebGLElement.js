6b94704b251011bf666c426ea992a8e8b16a88bd
WarperSan <leumas.ecole@gmail.com>
2025-05-24 16:37:27 -0400

    c

--- scripts/elements/WebGLElement.js
+++ scripts/elements/WebGLElement.js
@@ -1,113 +1,140 @@
-function onPlayerLoaded(player, foreground) {
+function setupGamePlayer(player, overlay) {
     if (!player.hasAttribute("src"))
         return;
 
     const iframeDoc = player.contentDocument || player.contentWindow.document;
-    const game = iframeDoc.querySelector("canvas");
+    const gameCanvas = iframeDoc.querySelector("canvas");
 
-    game.addEventListener("blur", () => {
-        foreground.style.display = "block";
-        foreground.style.pointerEvents = "none";
+    if (!gameCanvas)
+        return;
+
+    gameCanvas.addEventListener("blur", () => {
+        overlay.style.display = "block";
+        overlay.style.pointerEvents = "none";
     });
 
-    game.addEventListener("focus", () => {
-        foreground.style.display = "none";
+    gameCanvas.addEventListener("focus", () => {
+        overlay.style.display = "none";
     });
 
-    game.classList.add("web-game-content");
-
-    game.style.width = "100%";
-    game.style.height = "100%";
-    game.style.aspectRatio = "16 / 9";
+    gameCanvas.classList.add("web-game-content");
+    Object.assign(gameCanvas.style, {
+        width: "100%",
+        height: "100%",
+        aspectRatio: "16 / 9",
+    });
 }
 
-function createControl(classes, title, callback) {
-    const button = document.createElement("i");
+function createIcon(classNames, title) {
+    const icon = document.createElement("i");
+    icon.className = classNames;
+    icon.title = title;
+    return icon;
+}
 
-    button.className = classes;
-    button.title = title;
-    button.addEventListener("click", callback);
-
+function createButton(classNames, title, onClick) {
+    const button = createIcon(classNames, title);
+    button.addEventListener("click", onClick);
     return button;
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    document.querySelectorAll('webgl').forEach(el => {
-        // ATTRIBUTES
-        const src = window.absoluteSrc(el.getAttribute("src"));
-        const mobile = el.getAttribute("mobile");
-        const pc = el.getAttribute("pc");
+function createGameContainer(el) {
+    const src = window.absoluteSrc(el.getAttribute("src"));
+    const mobileAttr = el.getAttribute("mobile");
+    const pcAttr = el.getAttribute("pc");
 
-        // ELEMENTS
-        const container = document.createElement("div");
-        const player = document.createElement("iframe");
-        const foreground = document.createElement("div");
+    const isMobile = window.mobileCheck();
+    const isPlayable = (isMobile && mobileAttr != null) || (!isMobile && pcAttr != null);
 
-        // CONTAINER
-        container.classList.add("web-game");
+    const container = document.createElement("div");
+    container.classList.add("web-game");
 
-        // PLAYER
-        player.classList.add("web-game-player");
-        player.setAttribute("allow", "fullscreen");
-        player.setAttribute("allowfullscreen", true);
-        player.setAttribute("allowtransparency", true);
-        player.setAttribute("frameborder", 0);
-        player.setAttribute("scrolling", "no");
-        player.addEventListener("load", () => onPlayerLoaded(player, foreground));
-        player.style.aspectRatio = "16 / 9";
-        player.style.width = "100%";
-
-        // FOREGROUND
-        foreground.classList.add("web-game-foreground");
-
-        const isMobile = window.mobileCheck();
-        const isPlayable = isMobile && mobile != null || !isMobile && pc != null;
-
-        if (isPlayable) {
-            foreground.title = "Click to Play";
-            foreground.classList.add("play");
-            foreground.addEventListener("click", e => {
-                if (!player.hasAttribute("src"))
-                {
-                    ;
-                    player.setAttribute("src", src);
-                }
-
-                foreground.style.display = "none";
-            });
-        } else {
-            foreground.classList.add("error");
-            foreground.style.pointerEvents = "none";
-
-            const errorMessage = document.createElement("span");
-            errorMessage.textContent = "This game is not compatible with your platform";
-
-            foreground.appendChild(errorMessage);
-        }
-
-        // CONTROLS
-        const controls = document.createElement("div");
-        controls.classList.add("web-game-controls");
-        controls.style.display = isPlayable ? "inherit" : "none";
-
-        const fullscreen = createControl("fa-solid fa-expand", "Fullscreen", () => {
-            if (player.requestFullscreen) {
-                player.requestFullscreen();
-            } else if (player.mozRequestFullScreen) { /* Firefox */
-                player.mozRequestFullScreen();
-            } else if (player.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
-                player.webkitRequestFullscreen();
-            } else if (player.msRequestFullscreen) { /* IE/Edge */
-                player.msRequestFullscreen();
-            }
-        });
-
-        controls.appendChild(fullscreen);
-
-        container.appendChild(player);
-        container.appendChild(foreground);
-        container.appendChild(controls);
-
-        el.replaceWith(container);
+    const player = document.createElement("iframe");
+    player.classList.add("web-game-player");
+    Object.assign(player, {
+        allow: "fullscreen",
+        allowFullscreen: true,
     });
+    Object.assign(player.style, {
+        width: "100%",
+        aspectRatio: "16 / 9",
+    });
+    player.setAttribute("allowtransparency", "true");
+    player.setAttribute("frameborder", "0");
+    player.setAttribute("scrolling", "no");
+    player.addEventListener("load", () => setupGamePlayer(player, overlay));
+
+    const overlay = document.createElement("div");
+    overlay.classList.add("web-game-foreground");
+
+    if (isPlayable) {
+        overlay.title = "Click to Play";
+        overlay.classList.add("play");
+        overlay.addEventListener("click", () => {
+            if (!player.hasAttribute("src"))
+                player.setAttribute("src", src);
+
+            overlay.style.display = "none";
+        });
+    } else {
+        overlay.classList.add("error");
+        overlay.style.pointerEvents = "none";
+
+        const errorMessage = document.createElement("span");
+        errorMessage.textContent = "This game is not compatible with your platform";
+        overlay.appendChild(errorMessage);
+    }
+
+    /* CONTROLS */
+    const controls = document.createElement("div");
+    controls.classList.add("web-game-controls");
+    controls.style.display = isPlayable ? "inherit" : "none";
+
+    const leftControls = document.createElement("div");
+    Object.assign(leftControls.style, {
+        flex: 1,
+        display: "flex",
+        flexDirection: "row",
+        gap: "1em"
+    });
+
+    if (pcAttr != null) {
+        const pcIcon = createIcon("fa-solid fa-desktop", "PC");
+        leftControls.appendChild(pcIcon);
+    }
+
+    if (mobileAttr != null) {
+        const mobileIcon = createIcon("fa-solid fa-mobile-screen", "Mobile");
+        leftControls.appendChild(mobileIcon);
+    }
+
+    const rightControls = document.createElement("div");
+    Object.assign(rightControls.style, {
+        flex: 1,
+        display: "flex",
+        flexDirection: "row-reverse",
+        gap: "1em"
+    });
+
+    const fullscreenButton = createButton("fa-solid fa-expand", "Fullscreen", () => {
+        const reqFullscreen = player.requestFullscreen
+            || player.mozRequestFullScreen
+            || player.webkitRequestFullscreen
+            || player.msRequestFullscreen;
+
+        if (reqFullscreen)
+            reqFullscreen.call(player);
+    });
+
+    rightControls.appendChild(fullscreenButton);
+
+    controls.appendChild(leftControls);
+    controls.appendChild(rightControls);
+
+    container.append(player, overlay, controls);
+    el.replaceWith(container);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll("webgl").forEach(createGameContainer);
 });
