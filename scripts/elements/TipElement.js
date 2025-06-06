0d7e37792b2a00585c2554b9aa852e7669a48ab5
WarperSan <leumas.ecole@gmail.com>
2025-06-06 11:11:16 -0400

    Updated site

---
+++ scripts/elements/TipElement.js
@@ -0,0 +1,32 @@
+function createTip(el) {
+    const container = document.createElement("p");
+
+    container.innerText = el.innerText;
+
+    container.classList.add("tip");
+
+    return container;
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('help').forEach(el => {
+        const tip = createTip(el);
+        tip.classList.add("help");
+
+        el.replaceWith(tip);
+    });
+
+    document.querySelectorAll('warning').forEach(el => {
+        const tip = createTip(el);
+        tip.classList.add("warning");
+
+        el.replaceWith(tip);
+    });
+
+    document.querySelectorAll('alert').forEach(el => {
+        const tip = createTip(el);
+        tip.classList.add("alert");
+
+        el.replaceWith(tip);
+    });
+});
