70c0d02a38ea0e33310f1b0e1496bf55f9a7e05e
warpersan <leumas.ecole@gmail.com>
2026-02-02 13:24:17 -0500

    Fixed icons when games

--- scripts/elements/HeaderElement.js
+++ scripts/elements/HeaderElement.js
@@ -1,34 +1,15 @@
-function createIcon(url, classes, title) {
-    const link = document.createElement("a");
-    link.href = window.absoluteSrc(url);
-    link.classList.add("headerIcon");
-    link.title = title;
-    
-    const icon = document.createElement("i");
-    icon.className = classes;
-
-    const label = document.createElement("span");
-    label.innerText = title;
-
-    link.appendChild(icon);
-    link.appendChild(label);
-
-    return link;
-}
-
 class Header extends HTMLElement {
     connectedCallback() {
         const header = document.createElement("header");
         const icons = document.createElement("div");
 
-        const home = createIcon("index.html", "fa-solid fa-house", "Home");
-        const aboutMe = createIcon("pages/about-me.html", "fa-solid fa-user", "About Me");
-        const articles = createIcon("pages/articles/index.html", "fa-solid fa-newspaper", "Articles");
-        const games = createIcon("pages/games/index.html", "fa-solid fa-dice", "Games");
-        const tools = createIcon("pages/tools/index.html", "fa-solid fa-wrench", "Online Tools");
-        
-        icons.id = "headerIcons";
+        const home = this.createIcon("index.html", "fa-solid fa-house", "Home");
+        const aboutMe = this.createIcon("pages/about-me.html", "fa-solid fa-user", "About Me");
+        const articles = this.createIcon("pages/articles/index.html", "fa-solid fa-newspaper", "Articles");
+        const games = this.createIcon("pages/games/index.html", "fa-solid fa-dice", "Games");
+        const tools = this.createIcon("pages/tools/index.html", "fa-solid fa-wrench", "Online Tools");
 
+        icons.id = "headerIcons";
 
         icons.appendChild(home);
         icons.appendChild(aboutMe);
@@ -39,6 +20,24 @@ class Header extends HTMLElement {
         header.appendChild(icons);
 
         this.replaceWith(header);
+    }
+
+    createIcon(url, classes, title) {
+        const link = document.createElement("a");
+        link.href = window.absoluteSrc(url);
+        link.classList.add("headerIcon");
+        link.title = title;
+
+        const icon = document.createElement("i");
+        icon.className = classes;
+
+        const label = document.createElement("span");
+        label.innerText = title;
+
+        link.appendChild(icon);
+        link.appendChild(label);
+
+        return link;
     }
 }
 
