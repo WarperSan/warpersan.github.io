1efdaee89a9522f33bc4cfd536e8f4b8dcbf2304
WarperSan <leumas.ecole@gmail.com>
2026-03-16 12:16:28 -0400

    test5

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,20 @@
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
 			pages: 'build',
 			assets: 'build',
 			fallback: undefined,
 			precompress: false,
 			strict: true
-		})
-	},
-	basePaths: {
-		base: dev ? '' : '/warpersan.github.io' 
+		}),
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? '/warpersan.github.io' : '',
+		}
 	},
 	vitePlugin: {
 		dynamicCompileOptions: ({ filename }) =>
@@ -23,3 +23,4 @@ const config = {
 };
 
 export default config;
+
