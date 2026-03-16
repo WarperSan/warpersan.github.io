cf2e09a5addb5ef278e1b916ffda908fbb722f43
WarperSan <leumas.ecole@gmail.com>
2026-03-16 13:21:43 -0400

    Added Warning tip

---
+++ src/lib/components/Tips/WarningTip.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import type { Snippet } from "svelte";
+    import Tip from "./Tip.svelte";
+
+    interface Props {
+        children?: Snippet;
+    }
+
+    let { children }: Props = $props();
+</script>
+
+<Tip border="#f5d19d" background="#3B2B29">
+    <strong>Warning: </strong>
+    {@render children?.()}
+</Tip>
