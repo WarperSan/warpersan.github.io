a688a525483b18e365b4ff47b933b3d87ddc77cd
WarperSan <leumas.ecole@gmail.com>
2026-03-16 12:06:20 -0400

    try svelte

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import favicon from '$lib/assets/favicon.ico';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<custom-header></custom-header>
+
+{@render children()}
+
+<custom-footer></custom-footer>
